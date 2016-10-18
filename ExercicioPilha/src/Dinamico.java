05ebfa09040a626e33555ab82880bf30d10bdcb0
Matheus Henrique <matheus_hps@hotmail.com>
2016-10-18 13:30:09 -0200

    Atualizado a adição de elemetros na Lista
    Ainda não implementado a pilha

--- ExercicioPilha/src/Dinamico.java
+++ ExercicioPilha/src/Dinamico.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
 
 public class Dinamico {
-
+ public static void main(String[] args) {
+	
+	 
+	DinamicoLista lista = new DinamicoLista();
+	int quantidade;
+	Scanner teclado = new Scanner(System.in);
+	System.out.println("Digite a quantidade a ser empilhada:");
+	quantidade = teclado.nextInt();
+	
+	for (int i = quantidade; i < lista.indice; i++) {
+	System.out.println("Digite um valor:");
+	lista.valor = teclado.next();
+	lista.indice++;
+	}
+}
 }
